fbdba07450cc7516a5853a6f912b8ec69936be78
Zherdev Igor <cardinal3300@.com>
2025-12-13 12:22:01 +0300

    Добавлена модель Платежи

--- lessons/models.py
+++ lessons/models.py
@@ -4,6 +4,7 @@ from courses.models import Course
 
 
 class Lesson(models.Model):
+    """Модель Урок"""
     course = models.ForeignKey(
         Course, on_delete=models.CASCADE, related_name="lessons", verbose_name="Курс"
     )
--- users/models.py
+++ users/models.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
+from courses.models import Course
+from lessons.models import Lesson
 
 
 class User(AbstractUser):
+    """Модель Пользователь"""
     email = models.EmailField(unique=True, verbose_name="Email")
     phone = models.CharField(
         max_length=20, blank=True, null=True, verbose_name="Телефон"
@@ -16,3 +20,53 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    """Модель Платежи"""
+    CASH = 'cash'
+    TRANSFER = 'transfer'
+
+    PAYMENT_METHOD_CHOICES = ((CASH, 'Наличные'), (TRANSFER, 'Перевод на счет'))
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='payments',
+        verbose_name='Пользователь'
+    )
+    payment_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата оплаты'
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='payments',
+        verbose_name='Оплаченный курс'
+    )
+    lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='payments',
+        verbose_name='Оплаченный урок'
+    )
+    amount = models.PositiveIntegerField(
+        verbose_name='Сумма оплаты'
+    )
+    payment_method = models.CharField(
+        max_length=10,
+        choices=PAYMENT_METHOD_CHOICES,
+        verbose_name='Способ оплаты'
+    )
+
+    def __str__(self):
+        return f'{self.payment_date}: {self.user} — {self.amount} ({self.payment_method})'
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
