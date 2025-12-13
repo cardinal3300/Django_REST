0c2d97daccccd90635451aaccde4448ec13ad53b
Zherdev Igor <cardinal3300@.com>
2025-12-13 13:47:13 +0300

    Добавлена вложенность колличества уроков в сериализатор Курса.

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
 from courses.models import Course
+from lessons.serializers import LessonSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
