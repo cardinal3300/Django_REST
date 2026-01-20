e82d79afbd8ccf00ca4abaf0469419750ad4bf2e
Zherdev Igor <cardinal3300@.com>
2026-01-20 16:31:50 +0300

    начало подготовки ДЗ_34.2, создан Readme

--- config/settings.py
+++ config/settings.py
@@ -1,22 +1,17 @@
 import os
 from datetime import timedelta
-
 from dotenv import load_dotenv
 from pathlib import Path
-
-
-load_dotenv()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = os.getenv("SECRET_KEY")
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 
@@ -26,8 +21,6 @@ DEBUG = True
 ALLOWED_HOSTS = ()
 
 AUTH_USER_MODEL = "users.User"
-
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
@@ -46,11 +39,13 @@ INSTALLED_APPS = (
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
     "rest_framework_simplejwt",
-    "drf_yasg",
     "corsheaders",
     "django_celery_beat",
+    "drf_yasg",
+
     "courses",
     "lessons",
     "users",
@@ -153,14 +148,17 @@ CORS_ALLOWED_ORIGINS = (
     "https://read-and-write.example.com",
 )
 
-CSRF_TRUSTED_ORIGINS = ("https://read-and-write.example.com",)
-
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
+CSRF_TRUSTED_ORIGINS = ("https://read-and-write.example.com",)
 
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
 CELERY_BROKER_URL = REDIS_URL
+CELERY_RESULT_BACKEND = REDIS_URL
+
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_BACKEND = REDIS_URL
+CELERY_RESULT_SERIALIZER = "json"
+
 CELERY_TIMEZONE = TIME_ZONE
